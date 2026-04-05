009c27f356925a38aaafa48fb53db9a60cce10af
Diegao <diegonoguerarec@gmail.com>
2026-04-05 14:54:05 -0300

    Implementación de crear reserva y algunas validaciones

--- code-with-quarkus/src/main/java/resource/ReservaResource.java
+++ code-with-quarkus/src/main/java/resource/ReservaResource.java
@@ -6,6 +6,7 @@ import entity.Reserva;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
@@ -23,5 +24,17 @@ public class ReservaResource {
     public Response listar () {
         List<Reserva> reservas = service.listar();
         return Response.ok(reservas).build();
+    }
+
+    @POST
+    public Response crear (Reserva reserva) {
+        boolean ok = service.crear(reserva);
+
+        if (ok) {
+            return Response.status(Response.Status.CREATED).build();
+        } else {
+            // No se pudo crear por alguna razón
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
+        }
     }
 }
--- code-with-quarkus/src/main/java/service/ReservaService.java
+++ code-with-quarkus/src/main/java/service/ReservaService.java
@@ -1,11 +1,14 @@
 package service;
 
+import java.time.LocalDate;
 import java.util.List;
 
+import entity.Camion;
 import entity.Reserva;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import jakarta.persistence.EntityManager;
+import jakarta.transaction.Transactional;
 
 @ApplicationScoped
 public class ReservaService {
@@ -16,5 +19,42 @@ public class ReservaService {
     public List<Reserva> listar () {
         return em.createQuery("SELECT r FROM Reserva r", Reserva.class)
             .getResultList();
+    }
+
+    @Transactional
+    public boolean crear (Reserva reserva) {
+        // Validaciones y creacion o error
+
+        Long camion_id = reserva.getCamion().getId();
+        Camion camion = em.find(Camion.class, camion_id);
+
+        // Validaciones sobre el camion de la reserva
+        if (camion == null) {
+            return false;// Camion no existe
+        } else if (camion.getBorrado() == true) {
+            return false;// Camion borrado
+        } else if (camion.getCapacidad() < reserva.getVolumen_de_transporte()) {
+            return false;// Capacidad del camion insuficiente
+        }
+
+        //Validar que las fechas no sean incorrectas
+        LocalDate desde = reserva.getFecha_desde();
+        LocalDate hasta = reserva.getFecha_hasta();
+
+        if (!desde.isBefore(hasta)) {
+            return false;// fecha_desde es igual o mayor que fecha_hasta
+        }
+
+        // Validar que el camion no tenga otras reservas en ese rango de fecha
+        // Si tiene una reserva PENDIENTE o APROBADA no se puede crear la reserva
+        // Si tiene una reserva CANCELADA se puede crear
+        // Siempre se crea una en estado pendiente
+
+        reserva.setEstado("PENDIENTE");
+
+        reserva.setCamion(camion);
+
+        em.persist(reserva);
+        return true;
     }
 }
